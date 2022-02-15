cc7c1a7bef3c6fa921d89ef4a8806c0e645ddaec
Zeeshan Anjum <zeerox92@gmail.com>
2022-02-15 14:34:57 +0000

    Test

--- AmplifyFrameworkDataStore/AppDelegate.swift
+++ AmplifyFrameworkDataStore/AppDelegate.swift
@@ -6,6 +6,9 @@
 //
 
 import UIKit
+import Amplify
+import AmplifyPlugins
+import AWSPluginsCore
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,7 +16,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        do {
+                // AmplifyModels is generated in the previous step
+                let dataStorePlugin = AWSDataStorePlugin(modelRegistration: AmplifyModels())
+                try Amplify.add(plugin: dataStorePlugin)
+                try Amplify.add(plugin: AWSAPIPlugin())
+                try Amplify.configure()
+                print("Amplify configured with DataStore plugin")
+            } catch {
+                print("Failed to initialize Amplify with \(error)")
+                return false
+            }
         return true
     }
 
--- AmplifyFrameworkDataStore/ViewController.swift
+++ AmplifyFrameworkDataStore/ViewController.swift
@@ -6,14 +6,32 @@
 //
 
 import UIKit
+import Amplify
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var nameTF: UITextField!
+    @IBOutlet weak var descriptionTF: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func addButtonPressed(_ sender: Any) {
+        self.nameTF.resignFirstResponder()
+        self.descriptionTF.resignFirstResponder()
+        let profile = Profile.init(name: nameTF.text ?? "", description: descriptionTF.text ?? "");
 
+        Amplify.DataStore.save(profile) { result in
+            switch result {
+            case .success:
+                print("Profile saved successfully!")
+                self.nameTF.text = ""
+                self.descriptionTF.text = ""
+            case .failure(let error):
+                print("Error saving post \(error)")
+            }
+        }
+    }
+    
 }
 
